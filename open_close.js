59dd380d9bca22c56185ca1acdae3a4ef6dcdefd
ancura <Johanbaezcruz@gmail.com>
2016-03-04 10:20:17 -0500

    added Today's hours in my site

--- open_close.js
+++ open_close.js
@@ -1,14 +1,1 @@
-var checkOpenStatus = function () {
-    var d = new Date();
-    var date = d.getHours();
-    var day = d.getDay();
-    if ((date > 9) && (date < 20) && (day != 0)) {
-        y = "<span style=\"color:#07ed11\">We're Open!</span>";
-    } else {
-        y = "<span style=\"color:#fc4b1c\">Sorry we're Closed.</span>";
-    }
-    document.getElementById("open-close").innerHTML = y;
-    setTimeout(checkOpenStatus,15000);
-};
 
-checkOpenStatus();
